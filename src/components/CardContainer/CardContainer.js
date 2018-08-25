2a4179179819890b76eb97acb6af1c4bccc87e4c
Tim Fischer <emailtimfischer@gmail.com>
2018-08-25 15:52:18 -0600

    Add no favorites text

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -3,7 +3,7 @@ import './CardContainer.css';
 import Card from '../Card/Card.js'
 
 const CardContainer = ({ displayData, selectFavorite, crawlingText, removeFavorite, favoritesDisplaying, isFavoriteSelected, favorites }) => {
-  console.log(displayData);
+  console.log(favorites);
   const { episode, title, scrawl } = crawlingText;
   if (displayData.length > 0) {
     return (
@@ -16,7 +16,21 @@ const CardContainer = ({ displayData, selectFavorite, crawlingText, removeFavori
           favorites={favorites} />
       </div>
     )
-  } return (
+  }
+
+  if (favorites.length === 0 && favoritesDisplaying === true) {
+    return (
+      <div className="empty-wrapper">
+        <div className="empty-greeting">
+          <h1>No Favorites Selected</h1>
+        </div>
+      </div>
+    )
+  }
+
+
+
+  return (
     <div className="card-container">
       <div className="fade"></div>
       <section className="star-wars">
