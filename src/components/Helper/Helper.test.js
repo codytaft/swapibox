519697943e3aed1de468e58938940ae4e0995598
Cody Taft <cody.taft@gmail.com>
2018-08-25 13:07:16 -0600

    Begin helper tests

--- src/components/Helper/Helper.test.js
+++ src/components/Helper/Helper.test.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import {cleanHomeworld, cleanPlanetData} from './Helper'
+import {cleanHomeworld, cleanPlanetData, getOpeningScrawl} from './Helper'
 import { shallow } from 'enzyme'
 import {films} from '../../data/MockData.js'
 
 describe('cleanHomeWorld', () => {
 
-  it.only('should take in the correct data', () => {
+  it.only('should return the correct data', () => {
     let expected = {
       title: films.title,
       episode: films.episode,
       scrawl: films.scrawl
     }
-    getOpeningScrawl(films)
-    expect(getOpeningScrawl).toEqual(expected)
+    // getOpeningScrawl(films)
+    expect(getOpeningScrawl(films)).toEqual(expected)
   })
 })
