ba193ec159c423cd632c4f05f2c3eefac583cb72
Chris Boylen <billpolster@gmail.com>
2018-08-25 21:40:15 -0600

    Add and pass test so that handleclick invokes getpeopledata when value is people

--- src/components/Nav/Nav.test.js
+++ src/components/Nav/Nav.test.js
@@ -1,4 +1,52 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Nav from '../Nav/Nav';
+import Button from '../Button/Button';
 
+
+describe('Nav', () => {
+  let wrapper;
+  let wrapperButton;
+  let mockGetPeopleData;
+  let mockGetVehicleData;
+  let mockGetPlanetData;
+  let mockHandleClick;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <Nav 
+        // handleClick={mockHandleClick} 
+        getPeopleData={mockGetPeopleData}
+        getVehicleData={mockGetVehicleData}
+        getPlanetData={mockGetPlanetData}
+      />
+    );
+    // wrapperButton = shallow(
+    //   <Button 
+    //     getPeopleData={mockGetPeopleData}
+    //     getVehicleData={mockGetVehicleData}
+    //     getPlanetData={mockGetPlanetData}
+    //   />
+    // );
+    mockHandleClick = jest.fn();
+    mockGetPeopleData = jest.fn();
+    mockGetVehicleData = jest.fn();
+    mockGetPlanetData = jest.fn();
+  });
+
+  it('Should match snapshot', () => {
+    
+    expect(wrapper.html()).toMatchSnapshot()
+  });
+
+  describe('handleClick', () => {
+    it('Should invoke getPeopleData when value is people', () => {
+      const event = {target: {value: 'People'}};
+      const spy = jest.spyOn(wrapper.instance(), "handleClick");
+      wrapper.instance().forceUpdate();
+      wrapper.find('[name="People"]').simulate('click', event);
+
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+});
