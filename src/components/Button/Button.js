f312afc11fbe05b36eff4fecd8095c91dba78414
Tim Fischer <emailtimfischer@gmail.com>
2018-08-26 14:06:21 -0600

    Change button to stateful component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames/bind';
 import './Button.css';
 
-const Button = ({ onClick, name, favorites }) => {
-  const buttonClass = classNames({ [`${name}`]: true });
+// const Button = ({ onClick, name, favorites }) => {
 
-  return (
-    <button className={buttonClass} onClick={onClick} value={name}>
-      {name}
-    </button>
-  );
-};
+// const buttonClass = classNames({ [`${name}`]: true });
+
+//   return (
+//     <button className={buttonClass} onClick={onClick} value={name}>
+//       {name}
+//     </button>
+//   );
+// };
+
+class Button extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+    const buttonClass = classNames({ [`${this.props.name}`]: true });
+    return (
+      <div>
+        <button className={buttonClass} onClick={this.props.onClick} value={this.props.name}>
+          {this.props.name}
+        </button>
+      </div>)
+  }
+
+
+}
 export default Button;
