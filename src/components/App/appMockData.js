440cc68713ca97a4ee9eb8404b6a6260af6f8688
Cody Taft <cody.taft@gmail.com>
2018-08-26 15:50:27 -0600

    Test card component

--- src/components/App/appMockData.js
+++ src/components/App/appMockData.js
@@ -8,6 +8,33 @@ export const appMockData = [
   }
 ];
 
+export const mockCardData = [ { birth_year: '19BBY',
+created: '2014-12-09T13:50:51.644000Z',
+edited: '2014-12-20T21:17:56.891000Z',
+eye_color: 'blue',
+films:
+ [ 'https://swapi.co/api/films/2/',
+   'https://swapi.co/api/films/6/',
+   'https://swapi.co/api/films/3/',
+   'https://swapi.co/api/films/1/',
+   'https://swapi.co/api/films/7/' ],
+gender: 'male',
+hair_color: 'blond',
+height: '172',
+homeworld: 'https://swapi.co/api/planets/1/',
+mass: '77',
+name: 'Luke Skywalker',
+skin_color: 'fair',
+species: [ 'https://swapi.co/api/species/1/' ],
+starships:
+ [ 'https://swapi.co/api/starships/12/',
+   'https://swapi.co/api/starships/22/' ],
+stats: { Homeworld: [Object], Population: [Object], Species: [Object] },
+url: 'https://swapi.co/api/people/1/',
+vehicles:
+ [ 'https://swapi.co/api/vehicles/14/',
+   'https://swapi.co/api/vehicles/30/' ] } ]
+
 export const expectedAppMock = [{
   "birth_year": "19BBY",
   "created": "2014-12-09T13:50:51.644000Z",
--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
 import { shallow, mount } from 'enzyme';
 import Card from '../Card/Card';
-import { appMockData } from '../app/appMockData';
+import { mockCardData } from '../app/appMockData';
 
 describe('Card', () => {
   let wrapper;
-  let mockPeople = appMockData;
   let selectFavorite = jest.fn();
   let mockFavorites = [];
-  let mockButtonClass = cardbtn;
 
+  beforeEach(() => {
+    wrapper = shallow(<Card displayData={mockCardData} selectFavorite={selectFavorite} favorites={mockFavorites} />);
+  })
 
   it('should match the snapshot', () => {
-
-    wrapper = shallow(<Card displayData={mockPeople} selectFavorite={selectFavorite} favorites={mockFavorites} />);
-
     expect(wrapper).toMatchSnapshot();
-
   });
 
 
-  it('should display the card catergory with the corresponding data', () => {
-    // wrapper = mount(<Card displayData={mockDisplayData} selectFavorite={selectFavorite} mockFavorites={mockFavorites} />);
-    // console.log(wrapper.results.length);
+  it('should render a card', () => {
+    const result= wrapper.find('.card').length;
+    expect(result).toEqual(1)
   });
 });
