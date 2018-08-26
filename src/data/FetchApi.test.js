fcc8761a06f3fba60ac71460f6e6e5b4c3465191
Cody Taft <cody.taft@gmail.com>
2018-08-25 19:47:18 -0600

    Test fetchPlanetData

--- src/data/FetchApi.test.js
+++ src/data/FetchApi.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { fetchScrawl, fetchPeopleData, fetchVehicleData, fetchResidents } from './FetchApi';
+import { fetchScrawl, 
+        fetchPeopleData, 
+        fetchVehicleData, 
+        fetchResidents, 
+        fetchPlanetData,
+        fetchSpecies } from './FetchApi';
 import { shallow } from 'enzyme';
-import { getOpeningScrawl, cleanPeopleData, cleanSpecies, cleanVehicles } from '../components/Helper/Helper.js';
-import { MockData, mockFilms, mockVehicles, mockResidentLinks, mockResidentNames } from './MockData';
-// jest.mock('./FetchApi.js')
+import { cleanVehicles } from '../components/Helper/Helper.js';
+import { MockData, 
+        mockFilms, 
+        mockVehicles, 
+        mockResidentLinks, 
+        mockResidentNames,
+        mockPlanets, } 
+        from './MockData';
+import { fetchSpecies } from '../components/App/__mocks__/FetchApi';
+// import { fetchPlanetData } from '../components/App/__mocks__/FetchApi';
 
 describe('fetchScrawl', () => {
 
@@ -35,13 +47,40 @@ describe('fetchPeopleData', () => {
   });
 })
 
-describe('fetchResidents', () => {
+// describe('fetchResidents', () => {
 
-  it('Should return an array', async () => {
-    const result = await fetchResidents(mockResidentLinks)
-    expect(result).toEqual(mockResidentNames)
-  });
+//   it('Should return an array', async () => {
+//     const result = await fetchResidents(mockResidentLinks)
+//     expect(result).toEqual(mockResidentNames)
+//   });
+// })
+
+describe('fetchPlanetData', () => {
+  it('should invoke fetch with correct parameters', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(mockPlanets)
+    }))
+
+    const url = 'https://swapi.co/api/planets/'
+    fetchPlanetData()
+    expect(window.fetch).toHaveBeenCalledWith(url)
+  })
+  it('Should retun an object of planet data', async () => {
+    const result = await fetchPlanetData()
+    expect(result).toEqual(mockPlanets)
+  })
 })
+
+describe('fetchSpecies', () => {
+  it('should invoke fetch with the correct parameters', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(MockData.people)
+    }))
+    fetchSpecies()
+
+  })
+})
+
 
 describe('fetchVehicleData', () => {
 
@@ -49,6 +88,7 @@ describe('fetchVehicleData', () => {
     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       json: () => Promise.resolve(mockVehicles)
     }))
+
     const url = `https://swapi.co/api/vehicles/`
     fetchVehicleData()
     expect(window.fetch).toHaveBeenCalledWith(url)
